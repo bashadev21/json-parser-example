36500a2bf3c29eff0ef5bf1a15d6e4d1d677a5ac
thulasineom <thulasi.rajan@neom.com>
2022-04-08 01:45:23 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  bool isload = false;
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getdata();
+  }
+
+  List<Test>? tests;
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: isload
+            ? CircularProgressIndicator()
+            : ListView.builder(
+                itemCount: tests!.length,
+                itemBuilder: (ctx, index) {
+                  return tests![index].completed == false &&
+                          tests![index].userId!.isOdd
+                      ? ListTile(
+                          title: Text(tests![index].title.toString()),
+                        )
+                      : SizedBox();
+                }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void getdata() async {
+    isload = true;
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/');
+    await http
+        .get(
+      url,
+    )
+        .then((value) {
+      setState(() {});
+      tests = testFromJson(value.body);
+      isload = false;
+    });
+
+    // return testFromJson();
+  }
+}
+// To parse this JSON data, do
+//
+//     final test = testFromJson(jsonString);
+
+List<Test> testFromJson(String str) =>
+    List<Test>.from(json.decode(str).map((x) => Test.fromJson(x)));
+
+String testToJson(List<Test> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Test {
+  Test({
+    this.userId,
+    this.id,
+    this.title,
+    this.completed,
+  });
+
+  int? userId;
+  int? id;
+  String? title;
+  bool? completed;
+
+  factory Test.fromJson(Map<String, dynamic> json) => Test(
+        userId: json["userId"],
+        id: json["id"],
+        title: json["title"],
+        completed: json["completed"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "userId": userId,
+        "id": id,
+        "title": title,
+        "completed": completed,
+      };
+}
